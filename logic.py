6b671a51836b8294c7877362aab74237417245fa
Elmo675 <40466868+Elmo675@users.noreply.github.com>
2019-06-11 05:27:10 +0200

    Worker_Menager not Working, big Commit, almost done

--- logic.py
+++ logic.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-from multiprocessing import Process, Manager, Value, cpu_count
+from multiprocessing import Process,Queue, Manager, Value, cpu_count
 from functions import grab_text 
-
 
 #Classes
 class Task:
@@ -9,39 +8,90 @@ class Task:
         #initialize the variables
         self.func = grab_text if Type == 'text' else grab_photo
         self.url  = Url
-        self.result = Manager().list()
+        self.manager = Manager()
+        self.result = self.manager.list()
+        self.id = id(self)
         
         # status determine the progress of task
         #-1 = Error while handling the site connection
         # 0 = Task created  
         # 1 = Connection completed
         # 2 = URL Text formated 
-        # 3 = \<>?
+        # 3 = Task Done
         self.status = Value('i',0)
-    def begin(self):
+
+    def start(self):
         Process(target = self.func , args = self.get_args()).start()
+
     def get_args(self):
         return [self.url,self.status,self.result]
+
     def get_result(self):
         return self.result
-    
+
     def get_status(self):
         return self.status.value
 
+    def get_id(self):
+        return self.id
+
+class Task_Menager:
+    def __init__(self,cpu_count,que,done):
+        self.cpu_count = Value('i',cpu_count)
+#        self.manager = Manager() 
+#        self.que = self.manager.list()
+#        self.done = self.manager.list()
+        self.done = done
+        self.que = Queue()
+        Process(target = run , args = [self.que,self.done,self.cpu_count]).start()
+
+    def add_task(self,task):
+        self.que.put(task)
+        self.number +=1
+        
+    def get_result(self):
+        return self.done
+
+
+    
+def run(que,done,cpu_count):
+    for ii in range(10000):
+        for i in range(len(que)):
+            item = que[i]
+            status = item.get_status()
+            if status == 0 and cpu_count.value >0:
+                item.start()
+                cpu_count.value -=1
+            if status == 3:
+                cpu_count.value +=1
+                done.append([self.que[count].get_id(),self.que[count].get_result()])
+                del que[i]
+            if status == -1:
+                del que[i]
 
         
-    
-    
-
-
-if __name__ == '__main__':
+def test1():
     test = Task('text','https://www.quora.com/How-can-I-extract-only-text-data-from-HTML-pages')
-    test.begin()
+    test.start()
     while (test.get_status() !=3):
-        None
+        print (test.get_status())
     result = test.get_result()
     for i in range(len(result)):
         print (result[i])
+
+def test2():
+    manager = Manager() 
+    que = manager.list()
+    done = manager.list()
+    test = Task_Menager(2,que,done)
+    test.add_task(Task('text','https://www.quora.com/How-can-I-extract-only-text-data-from-HTML-pages'))
+    for _ in range(1000):
+        print(test.get_result())
+
+
+if __name__ == '__main__':
+    test2()
+    
         
     
     
--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from json import dumps
-
+from logic import Todo
 app = Flask(__name__)
 api = Api(app)
 
@@ -17,20 +17,19 @@ class Order(Resource):
     
 class Order_Result(Resource):
     def get(self, order_id):
-        return None
+        return {
 
     
 class Order_Status(Resource):
     def get(self, order_id):
-        return None
+        return {'status':'success'}
 
 
 api.add_resource(Order, '/order') # Route_1
 api.add_resource(Order_Status, '/order-status/<order_id>') # Route_2
-api.add_resource(Order_Result, '/order-result/<order_id>') # Route_3
+api.add_resource(Order_Result, '/order-result/<order_id>') # Route_2
 
 
 
 if __name__ == '__main__':
      app.run()
-
