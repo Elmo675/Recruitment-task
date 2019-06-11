001ec96775e5f2f5a223728cab33ab2b880a581a
Elmo675 <40466868+Elmo675@users.noreply.github.com>
2019-06-11 09:20:40 +0200

    V 1.0
    stable version with known issues to update

--- logic.py
+++ logic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-from multiprocessing import Process,Queue, Manager, Value, cpu_count
-from functions import grab_text 
+from multiprocessing import Process, Manager, Value, cpu_count
+from functions import grab_text,grab_photo 
 
 #Classes
 class Task:
@@ -10,16 +10,20 @@ class Task:
         self.url  = Url
         self.manager = Manager()
         self.result = self.manager.list()
-        self.id = id(self)
         
         # status determine the progress of task
+        #-2 = Cannot make new folder
         #-1 = Error while handling the site connection
         # 0 = Task created  
         # 1 = Connection completed
-        # 2 = URL Text formated 
+        # 2 = Url formated 
         # 3 = Task Done
+        self.INFO = ["Can't make new folder","Error while handling the site connection",
+                     "Task created","Connection completed",
+                     "Url formated","Task Done"]
         self.status = Value('i',0)
-
+        self.start()
+        
     def start(self):
         Process(target = self.func , args = self.get_args()).start()
 
@@ -32,67 +36,80 @@ class Task:
     def get_status(self):
         return self.status.value
 
-    def get_id(self):
-        return self.id
 
-class Task_Menager:
-    def __init__(self,cpu_count,que,done):
-        self.cpu_count = Value('i',cpu_count)
+
+#class Task_Menager:
+#    def __init__(self,cpu_count):
+#        self.cpu_count = Value('i',cpu_count)
 #        self.manager = Manager() 
 #        self.que = self.manager.list()
 #        self.done = self.manager.list()
-        self.done = done
-        self.que = Queue()
-        Process(target = run , args = [self.que,self.done,self.cpu_count]).start()
-
-    def add_task(self,task):
-        self.que.put(task)
-        self.number +=1
-        
-    def get_result(self):
-        return self.done
+#        Process(target = run , args = [self.que,self.done,self.cpu_count]).start()
+#
+#    def add_task(self,task):
+#        self.que.put(task)
+#        self.number +=1
+#        
+#    def get_result(self):
+#        return self.done
 
 
     
-def run(que,done,cpu_count):
-    for ii in range(10000):
-        for i in range(len(que)):
-            item = que[i]
-            status = item.get_status()
-            if status == 0 and cpu_count.value >0:
-                item.start()
-                cpu_count.value -=1
-            if status == 3:
-                cpu_count.value +=1
-                done.append([self.que[count].get_id(),self.que[count].get_result()])
-                del que[i]
-            if status == -1:
-                del que[i]
+#def run(que,done,cpu_count):
+#    for ii in range(10000):
+#        for i in range(len(que)):
+#            item = que[i]
+#            status = item.get_status()
+#            if status == 0 and cpu_count.value >0:
+#                item.start()
+#                cpu_count.value -=1
+#            if status == 3:
+#                cpu_count.value +=1
+#                done.append([self.que[count].get_id(),self.que[count].get_result()])
+#                del que[i]
+#            if status == -1:
+#                del que[i]
 
         
-def test1():
-    test = Task('text','https://www.quora.com/How-can-I-extract-only-text-data-from-HTML-pages')
+def test_Task_Controll_Positive(Url,Type='text'):
+    test = Task(Type,Url)
     test.start()
     while (test.get_status() !=3):
-        print (test.get_status())
+        None
     result = test.get_result()
     for i in range(len(result)):
         print (result[i])
+    print()
+    print (test.get_status())
 
-def test2():
-    manager = Manager() 
-    que = manager.list()
-    done = manager.list()
-    test = Task_Menager(2,que,done)
-    test.add_task(Task('text','https://www.quora.com/How-can-I-extract-only-text-data-from-HTML-pages'))
-    for _ in range(1000):
-        print(test.get_result())
+def test_Task_Controll_Negative(Url,Type='text'):    
+    test = Task(Type,Url)
+    test.start()
+    while (test.get_status() >= 0):
+        None
+    print (test.get_status())
+#def test2():
+#    manager = Manager() 
+#    que = manager.list()
+#    done = manager.list()
+#    test = Task_Menager(2,que,done)
+#    test.add_task(Task('text','https://www.quora.com/How-can-I-extract-only-text-data-from-HTML-pages'))
+#    for _ in range(1000):
+#        print(test.get_result())
 
 
 if __name__ == '__main__':
-    test2()
-    
-        
+    #Test Purpose
+    print ("Test positive text")
+    test_Task_Controll_Positive('https://www.quora.com/How-can-I-extract-only-text-data-from-HTML-pages')
+    print ("Test positive image(no images)")
+    test_Task_Controll_Positive('https://www.quora.com/How-can-I-extract-only-text-data-from-HTML-pages','image')
+    print ("Test positive image (images downloaded)")
+    test_Task_Controll_Positive('https://pixabay.com/','image')
+    print ("Test negative text")
+    test_Task_Controll_Negative('https://www.quoradada.com/How-can-I-extract-only-text-data-from-HTML-pages')
+    print ("Test negative image")
+    test_Task_Controll_Negative('www.google.com','image')
     
     
     
